78dbcd86361021eae2e346f569d8e444a13befe4
Allan Almeida <almeida_allan8@hotmail.com>
2024-11-20 20:36:01 -0300

    Win condition refactor and add timer

--- Assets/Scripts/UI/CanvasController.cs
+++ Assets/Scripts/UI/CanvasController.cs
@@ -12,10 +12,14 @@ public class CanvasController : MonoBehaviour
     [SerializeField] private RectTransform losePanel;
     [SerializeField] private RectTransform winPanel;
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private TextMeshProUGUI winText;
     [SerializeField] private Button restartGame;
     [SerializeField] private Button returnButton;
     [SerializeField] private GameObject player;
-
+    
+    private float elapsedTime = 0f;
+    private bool isTimerRunning = true;
     private int playerscore = 0;
 
     void Awake()
@@ -28,6 +32,18 @@ public class CanvasController : MonoBehaviour
     {
         restartGame.onClick.AddListener(RestartGame);
         returnButton.onClick.AddListener(ReturnMainMenu);
+    }
+    private void Update()
+    {
+        if (!isTimerRunning) return;
+
+        elapsedTime += Time.deltaTime;
+
+        int minutes = Mathf.FloorToInt(elapsedTime / 60f);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60f);
+        int milliseconds = Mathf.FloorToInt((elapsedTime * 1000f) % 1000);
+
+        timerText.text = $"{minutes:D2}:{seconds:D2}:{milliseconds:D3}";
     }
 
     public void RestartGame()
@@ -47,6 +63,7 @@ public class CanvasController : MonoBehaviour
         if(playerscore >= 17)
         {
             player.SetActive(false);
+            winText.text = "you win in " + timerText.text + " try blindfolded now";
             winPanel.gameObject.SetActive(true);
         }
     }
